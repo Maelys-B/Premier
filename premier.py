027b4090062eed3c090045b0609c24de7b4dd285
Maelys-B <maelysbart@gmail.com>
2025-10-06 10:30:51 +0200

    ajout fonction

--- premier.py
+++ premier.py
@@ -1,9 +1,7 @@
-n=int(input("Entrez un nombre nombre: "))
-for diviseur in range (2,n):
-    reste = n % diviseur
-if reste == 0:
-    print (f"{n}, n'est pas premier car divisible par {diviseur}")
-    premier = False
-
-if premier:
-    print(n,"est premier")
+from utils import estPremier
+    
+n=int(input("Entrez un nombre: "))
+if estPremier(n):
+    print (n,"est premier")
+else:
+    print (n,"n'est pas premier")
